f3956d888d2905679c351ed7b54098f33c4ba69e
Subhajeet Muhuri <subhajeet.muhuri@gmail.com>
2023-10-02 09:15:08 +0530

    sm6375-common: Include hardware/google/interfaces and hardware/google/pixel
     * Required by power-libperfmgr
    
    Change-Id: I4053938614be26c7066ea83d97955739f39b4144

--- common.mk
+++ common.mk
@@ -293,6 +293,8 @@ PRODUCT_PACKAGES += \
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
+    hardware/google/interfaces \
+    hardware/google/pixel \
     hardware/motorola
 
 # Telephony
